fb6abcbdf436194f27a0a2cf7e06dcba125ca70e
EdvardsMekss <edvards.mekss@gmail.com>
2023-02-28 15:51:11 +0200

    Add clear function that clears entire screen

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+// Value stores number which was clicked by user and is added on screen
 function writeToScreen(value){
     screenText.innerText += value;
 }
 
+// Converts innerText to string and removes last element
 function backspace(){
     if(screenText.innerText.length !== 0){
         let currentNumber = screenText.innerText.toString();
         let newCurrNumb = currentNumber.slice(0, -1);
         screenText.innerText = newCurrNumb;
+    }
+}
+
+// Clears entire screen
+function clear(){
+    if(screenText.innerText.length !== 0){
+        screenText.innerText = "";
     }
 }
 
@@ -17,6 +26,12 @@ let backspaceButton = document.getElementById("backspace");
 backspaceButton.addEventListener('click', ()=>{
     backspace();
 });
+
+// Add event listener to clear button
+let clearButton = document.getElementById("clear");
+clearButton.addEventListener('click', ()=>{
+    clear();
+})
 
 // Add event listeners to all number buttons
 const numbers = document.getElementsByClassName("numb");
