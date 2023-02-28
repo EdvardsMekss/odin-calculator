da9c7487340055acbe1f10ee45ea53b45a71a43d
EdvardsMekss <edvards.mekss@gmail.com>
2023-02-28 15:26:03 +0200

    Add backspace function

--- script.js
+++ script.js
@@ -2,9 +2,23 @@ function writeToScreen(value){
     screenText.innerText += value;
 }
 
+function backspace(){
+    if(screenText.innerText.length !== 0){
+        let currentNumber = screenText.innerText.toString();
+        let newCurrNumb = currentNumber.slice(0, -1);
+        screenText.innerText = newCurrNumb;
+    }
+}
 
 let screenText = document.getElementById("screen-text");
 
+// Add event listener to backspace button
+let backspaceButton = document.getElementById("backspace");
+backspaceButton.addEventListener('click', ()=>{
+    backspace();
+});
+
+// Add event listeners to all number buttons
 const numbers = document.getElementsByClassName("numb");
 for(const number of numbers){
     number.addEventListener('click', ()=>{
@@ -12,4 +26,3 @@ for(const number of numbers){
         writeToScreen(value);
     })
 }
-
